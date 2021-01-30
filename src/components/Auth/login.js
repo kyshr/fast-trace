01c4fe7104203db7231463640621fea216f6f881
Kyle Joseph Timajo <timajokyle24@gmail.com>
2021-01-30 17:17:39 +0800

    Login form validation done

--- src/components/Auth/login.js
+++ src/components/Auth/login.js
@@ -1,9 +1,55 @@
-import React from "react";
+import React, {useState}from "react";
 import { Redirect } from "react-router-dom";
 import "../../assets/scss/login.scss";
 import logo from "../../assets/images/qr.jpg";
 
 const Login = ({ logged }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState({email: '', password: ''});
+
+    const handleChange = (event) => {
+        if(event.target.id === 'email'){
+            setError((err) => {return {...err, email: ''}});
+            setEmail(event.target.value);
+        }
+        else if(event.target.id === 'password'){
+            setError((err) => {return {...err, password: ''}});
+            setPassword(event.target.value);
+        }
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        handleError();
+        console.log(`${email} ${password}`);
+        console.log(error);
+    }
+
+    const handleError = () =>{
+        if(email === ''){
+            // setEmailErr('Email is required');
+            setError((err) => {return {...err, email: 'Email is required.'}});
+        }
+        else{
+            const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            if (re.test(String(email).toLowerCase())){
+                setError((err) => {return {...err, email: ''}});
+            }
+            else{
+                setError((err) => {return {...err, email: 'Invalid email.'}});
+            }
+        }
+
+        if(password === ''){
+            // setPasswordErr('Password is required');
+            setError((err) => {return {...err, password: 'Password is required.'}});
+        }
+        else if(password.length > 0 && password.length < 8){
+            setError((err) => {return {...err, password: 'Password should be greater than 8 characters.'}});
+        }
+    }
+
     if (logged) {
         return <Redirect to="/" />;
     } else {
@@ -31,26 +77,28 @@ const Login = ({ logged }) => {
                                                     tracing.
                                                 </p>
                                             </div>
-                                            <form>
+                                            <form onSubmit={handleSubmit}>
                                                 <div className="form-group first">
-                                                    <label htmlFor="username">
-                                                        Email
-                                                    </label>
                                                     <input
                                                         type="text"
                                                         className="form-control"
-                                                        id="username"
+                                                        id="email"
+                                                        placeholder="Email"
+                                                        value={email}
+                                                        onChange={handleChange}
                                                     />
+                                                    <p className='text-danger'>{error.email}</p>
                                                 </div>
                                                 <div className="form-group last mb-4">
-                                                    <label htmlFor="password">
-                                                        Password
-                                                    </label>
                                                     <input
                                                         type="password"
                                                         className="form-control"
                                                         id="password"
+                                                        placeholder="Password"
+                                                        value={password}
+                                                        onChange={handleChange}
                                                     />
+                                                    <p className='text-danger'>{error.password}</p>
                                                 </div>
 
                                                 <input
--- src/components/Auth/signup.js
+++ src/components/Auth/signup.js
@@ -9,7 +9,7 @@ const Signup = ({ logged }) => {
     } else {
         return (
             <>
-                <div style={{ paddingTop: "70px" }}>
+                <div style={{ paddingTop: "80px" }}>
                     <div className="content d-flex align-items-center py-md-5">
                         <div className="container px-4 px-md-0">
                             <div className="row">
@@ -33,33 +33,27 @@ const Signup = ({ logged }) => {
                                             </div>
                                             <form>
                                                 <div className="form-group first">
-                                                    <label htmlFor="username">
-                                                        Email
-                                                    </label>
                                                     <input
                                                         type="text"
                                                         className="form-control"
                                                         id="username"
+                                                        placeholder='Email'
                                                     />
                                                 </div>
-                                                <div className="form-group last">
-                                                    <label htmlFor="password">
-                                                        Password
-                                                    </label>
+                                                <div className="form-group">
                                                     <input
                                                         type="password"
                                                         className="form-control"
                                                         id="password"
+                                                        placeholder='Password'
                                                     />
                                                 </div>
                                                 <div className="form-group last mb-4">
-                                                    <label htmlFor="cpassword">
-                                                        Confirm Password
-                                                    </label>
                                                     <input
                                                         type="cpassword"
                                                         className="form-control"
                                                         id="cpassword"
+                                                        placeholder='Confirm Password'
                                                     />
                                                 </div>
 
