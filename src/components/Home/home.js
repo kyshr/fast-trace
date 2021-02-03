126f1da631fc21901e027934180a234b5de412d2
Kyle Joseph Timajo <timajokyle24@gmail.com>
2021-02-03 16:53:47 +0800

    QR Code Component Added

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react"
 import { Redirect } from "react-router-dom"
 import Loading from "../loading"
+import { QRCode } from "react-qr-svg"
 
 const Home = ({ logged }) => {
     const [loading, setLoading] = useState(true)
@@ -16,7 +17,15 @@ const Home = ({ logged }) => {
             <Loading />
         ) : (
             <div style={{ paddingTop: "70px" }}>
-                <h1>Home Page</h1>
+                <div className="container d-flex align-items-center justify-content-center pt-5">
+                    <QRCode
+                        bgColor="#FFFFFF"
+                        fgColor="#000000"
+                        level="H"
+                        style={{ width: 256 }}
+                        value="Kyle Joseph Timajo"
+                    />
+                </div>
             </div>
         )
     } else {
